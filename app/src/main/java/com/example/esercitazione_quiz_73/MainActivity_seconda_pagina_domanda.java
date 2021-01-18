069d365acd056995236f7ab06f2b1a7637d2a753
DaviLenti <davide.lentini.its2020@gmail.com>
2021-01-18 15:44:52 +0100

    modifica della posizione dei radio button e textview

--- app/src/main/java/com/example/esercitazione_quiz_73/MainActivity_seconda_pagina_domanda.java
+++ app/src/main/java/com/example/esercitazione_quiz_73/MainActivity_seconda_pagina_domanda.java
@@ -2,7 +2,12 @@ package com.example.esercitazione_quiz_73;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity_seconda_pagina_domanda extends AppCompatActivity {
@@ -12,7 +17,21 @@ public class MainActivity_seconda_pagina_domanda extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_seconda_pagina_domanda);
 
-        
+        RadioButton Risposta_corretta_seconda_pagina= findViewById(R.id.rispfosseroneutrali);
+        Button Button_next_seconda_pagina = findViewById(R.id.button_Next_seconda_pagina);
+        RadioGroup radiogroup= findViewById(R.id.radiogroupsecondapagina);
+
+        Button_next_seconda_pagina.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(Risposta_corretta_seconda_pagina.isChecked()){
+                    MainActivity_prima_pagina_domanda.Punteggio.val_pt++;
+                }
+                startActivity(new Intent(MainActivity_seconda_pagina_domanda.this,MainActivity_terza_pagina_domanda.class));
+                radiogroup.clearCheck();
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/example/esercitazione_quiz_73/MainActivity_terza_pagina_domanda.java
@@ -0,0 +1,17 @@
+package com.example.esercitazione_quiz_73;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity_terza_pagina_domanda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_terza_pagina_domanda);
+
+
+    }
+}
